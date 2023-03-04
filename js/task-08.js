dfc4b876f240300daa8aa44bac27ddc347d2cba7
Alexander <alexkorpol1964@gmail.com>
2023-03-04 21:42:49 +0200

    Correct task №8 (add return in function after if)

--- js/task-08.js
+++ js/task-08.js
@@ -15,13 +15,14 @@ function treatmentSubmit(event) {
     event.preventDefault();
 
     const {
-    elements: { email, password }
+        elements: { email, password }
     } = event.currentTarget;
     
     
     if (email.value === "" || password.value === "") {
-    alert("Please fill in all the fields!");
-    }
+        alert("Please fill in all the fields!");
+        return
+        }
     
     const outPut = { email: email.value, password: password.value };
     console.log("Вихідні дані завдання", outPut);
